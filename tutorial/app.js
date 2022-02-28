424af72bd555cde82716bec03857149d28d3e8c3
matiaswasiak <matiaswasiak@gmail.com>
2022-02-28 14:21:54 -0300

    altenative syntax

--- tutorial/app.js
+++ tutorial/app.js
@@ -4,6 +4,8 @@
 // const names = require("./4-names");
 const { john, peter } = require("./4-names");
 const sayHi = require("./5-utils");
+const data = require("./6-alternative-flavor");
+console.log(data);
 
 sayHi("susan");
 // sayHi(names.john);
