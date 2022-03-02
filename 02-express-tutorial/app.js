7e9f7e97c593c98e509493907f6d4e0aedc7cac0
matiaswasiak <matiaswasiak@gmail.com>
2022-03-02 13:01:38 -0300

    http - headers

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,9 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("user hit the server");
-  res.end("home page");
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.write("<h1>home page</h1>");
+  res.end();
 });
 
 server.listen(5000);
