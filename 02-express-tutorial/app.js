725ac604d921bb0c56c27804e27444bb08bf2b35
matiaswasiak <matiaswasiak@gmail.com>
2022-03-02 13:19:11 -0300

    http - html file

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,9 +1,29 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+// get all files
+const homePage = readFileSync("./index.html");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.write("<h1>home page</h1>");
-  res.end();
+  const url = req.url;
+  // home page
+  if (url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(homePage);
+    res.end();
+  }
+  // about page
+  else if (url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>about page</h1>");
+    res.end();
+  }
+  // 404
+  else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.write("<h1>page not found</h1>");
+    res.end();
+  }
 });
 
 server.listen(5000);
