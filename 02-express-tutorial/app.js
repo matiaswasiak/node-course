a7da780b7502680338180787499a581eb7976937
matiaswasiak <matiaswasiak@gmail.com>
2022-03-02 17:17:33 -0300

    route params

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,20 +1,38 @@
 const express = require("express");
-const path = require("path");
 const app = express();
+const { products } = require("./data");
 
-// setup static and middleware
-app.use(express.static("./public"));
+app.get("/", (req, res) => {
+  res.send("<h1>Home Page</h1><a href='/api/products'>products</a>");
+});
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-//   adding to static assets
-//   SSR
-// });
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
 
-app.all("*", (req, res) => {
-  res.status(404).send("resource not found");
+app.get("/api/products/:productID", (req, res) => {
+  // console.log(req);
+  // console.log(req.params);
+  const { productID } = req.params;
+
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("Product Does Not Exist");
+  }
+  return res.json(singleProduct);
+});
+
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  console.log(req.params);
+  res.send("hello world");
 });
 
 app.listen(5000, () => {
-  console.log("server is listening on port 5000...");
+  console.log("Server is listening on port 5000...");
 });
